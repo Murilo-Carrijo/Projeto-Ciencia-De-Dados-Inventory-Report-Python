f3a2f70cd19c7db5c97e186e46139b3b4f0c555e
murilo <murilocarrijoadm@gmail.com>
2022-08-12 17:27:28 -0300

    test: teste para o metodo __init__ da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_da_empresa="Xablau",
+        nome_do_produto="molho de tomate",
+        data_de_fabricacao="12/08/2022",
+        data_de_validade="12/08/2023",
+        numero_de_serie="123456789XB",
+        instrucoes_de_armazenamento="Utilizar todo o conteudo"
+    )
+    assert product.id == 1
+    assert product.nome_da_empresa == "Xablau"
+    assert product.nome_do_produto == "molho de tomate"
+    assert product.data_de_fabricacao == "12/08/2022"
+    assert product.data_de_validade == "12/08/2023"
+    assert product.numero_de_serie == "123456789XB"
+    assert product.instrucoes_de_armazenamento == "Utilizar todo o conteudo"
