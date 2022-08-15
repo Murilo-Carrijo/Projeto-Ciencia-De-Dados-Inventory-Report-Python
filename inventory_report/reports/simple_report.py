579f2b85f059813696457d53b112cf4d0dfb87c8
murilo <murilocarrijoadm@gmail.com>
2022-08-15 17:52:25 -0300

    fix: refatorando o nome dos metodos e removendo os prints da classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 class SimpleReport:
 
-    def oldest_manufacturing(products):
+    def get_oldest_manufacturing(products):
         oldest_product = datetime.today().strftime("%Y-%m-%D")
         for product in products:
             if product["data_de_fabricacao"] < oldest_product:
                 oldest_product = product["data_de_fabricacao"]
         return oldest_product
 
-    def shelf_life(products):
+    def get_shelf_life(products):
         today = datetime.today().strftime("%Y-%m-%D")
         shelf_product = "2025-10-17"
         for product in products:
@@ -19,15 +19,14 @@ class SimpleReport:
                     shelf_product = product["data_de_validade"]
         return shelf_product
 
-    def company_with_more_products(products):
+    def get_company_with_more_products(products):
         companies = list(product['nome_da_empresa'] for product in products)
-        print(companies)
         return max(companies, key=companies.count)
 
     def generate(products):
-        manufacturing = SimpleReport.oldest_manufacturing(products)
-        shelf = SimpleReport.shelf_life(products)
-        company = SimpleReport.company_with_more_products(products)
+        manufacturing = SimpleReport.get_oldest_manufacturing(products)
+        shelf = SimpleReport.get_shelf_life(products)
+        company = SimpleReport.get_company_with_more_products(products)
         return(
            f"Data de fabricação mais antiga: {manufacturing}\n"
            f"Data de validade mais próxima: {shelf}\n"
