9dc38ebee7a6e90b8361583b4257b8e72a8b5c22
murilo <murilocarrijoadm@gmail.com>
2022-08-16 19:56:30 -0300

    feat: implementando a classe Inventory com o metodo import_data para arquivos xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from xml.etree import ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -12,6 +13,16 @@ class Inventory:
                 data = list(file_csv)
             if path.endswith('json'):
                 data = json.load(file)
+            if path.endswith('xml'):
+                tree = ET.parse(path)
+                root = list(tree.getroot())
+                data = []
+                info_dict = {}
+                for i in range(len(root)):
+                    for info in root[i]:
+                        info_dict[info.tag] = info.text
+                    data.append(info_dict)
+                    info_dict = {}
 
         if type == 'simples':
             return SimpleReport.generate(data)
