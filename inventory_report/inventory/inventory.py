22a6a836242087f28de38ac17e3c29e0cbf2e3c9
murilo <murilocarrijoadm@gmail.com>
2022-08-16 21:15:02 -0300

    fix: reduzindo a complexidade do metodo import_data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,7 +6,19 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def import_data(path, type):
+    @classmethod
+    def format_data_xml(self, root):
+        data = []
+        info_dict = {}
+        for i in range(len(root)):
+            for info in root[i]:
+                info_dict[info.tag] = info.text
+            data.append(info_dict)
+            info_dict = {}
+        return data
+
+    @classmethod
+    def import_data(self, path, type):
         with open(path, encoding="utf-8") as file:
             if path.endswith('csv'):
                 file_csv = csv.DictReader(file)
@@ -16,13 +28,7 @@ class Inventory:
             if path.endswith('xml'):
                 tree = ET.parse(path)
                 root = list(tree.getroot())
-                data = []
-                info_dict = {}
-                for i in range(len(root)):
-                    for info in root[i]:
-                        info_dict[info.tag] = info.text
-                    data.append(info_dict)
-                    info_dict = {}
+                data = self.format_data_xml(root)
 
         if type == 'simples':
             return SimpleReport.generate(data)
